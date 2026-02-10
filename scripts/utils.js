48bdadb97b4311740079ce416d66649640a01016
Yipeng He <she50877@adobe.com>
2026-02-11 00:31:28 +0800

    fix: bug

--- scripts/utils.js
+++ scripts/utils.js
@@ -91,7 +91,7 @@ export const getBlockConfigs = async (block, defaults = {}, blockName = '') => {
             // 拿user输入的值。没有时，用配置的默认值
             console.log(row?.innerHTML.trim())
             let value = cell?.textContent.trim() || (config[fieldName] || '');
-            const html = row?.innerHTML.trim() || (config[fieldName] || '');
+            const html = cell?.innerHTML.trim() || (config[fieldName] || '');
             // If text content is empty, check for image/picture elements
             if (value === '') {
               const img = cell.querySelector('img');
