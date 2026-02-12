f6fd5d04c07578d87b54a4650240f63206670daa
Yu Luo <she87373@adobe.com>
2026-02-12 20:17:53 +0800

    feat: add commit lint

--- .husky/pre-commit.mjs
+++ .husky/pre-commit.mjs
@@ -8,13 +8,18 @@ const run = (cmd) => new Promise((resolve, reject) => exec(
   }
 ));
 
+//check lint
+const lintOutput = await run('npm run lint');
+console.log(lintOutput);
+
 const changeset = await run('git diff --cached --name-only --diff-filter=ACMR');
 const modifiedFiles = changeset.split('\n').filter(Boolean);
 
 // check if there are any model files staged
 const modifledPartials = modifiedFiles.filter((file) => file.match(/(^|\/)_.*.json/));
 if (modifledPartials.length > 0) {
-  const output = await run('npm run build --silent');
+  const output = await run('npm run build:json --silent');
   console.log(output);
   await run('git add component-models.json component-definition.json component-filters.json');
 }
+
