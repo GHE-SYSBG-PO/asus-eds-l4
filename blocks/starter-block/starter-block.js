9e1ed52b232a490e41acc508d9fc9a8cddd060dd
Rain Lin <s.rain@yahoo.com.tw>
2026-02-13 13:59:50 +0800

    feat: 轉換 starter-block 為卡片列表顯示功能，支援顯示多媒體內容與自動圖片優化

--- blocks/starter-block/starter-block.js
+++ blocks/starter-block/starter-block.js
@@ -1,32 +1,23 @@
-import { getBlockConfigs, getFieldValue } from '../../scripts/utils.js';
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
-// 1. 定義預設配置
-const DEFAULT_CONFIG = {
-  title: 'Default Title',
-  description: 'Default Description',
-  styleLayout: 'left', // left, center, right
-};
-
-export default async function decorate(block) {
-  // 2. 透過 L4 Utils 讀取配置 (這是核心)
-  const config = await getBlockConfigs(block, DEFAULT_CONFIG, 'starter-block');
-  const v = getFieldValue(config);
-
-  // 3. 提取資料
-  const title = v('title');
-  const desc = v('description');
-  const layout = v('styleLayout');
-
-  // 4. 清空原有內容並重建 DOM
-  block.innerHTML = '';
-
-  // 使用 Template Literal 建構 HTML
-  const html = `
-    <div class="starter-wrapper layout-${layout}">
-      <h2 class="starter-title">${title}</h2>
-      <div class="starter-desc">${desc}</div>
-    </div>
-  `;
-
-  block.innerHTML = html;
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    moveInstrumentation(row, li);
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
+  block.replaceChildren(ul);
 }
