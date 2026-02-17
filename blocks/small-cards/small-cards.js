9dc83cf5a1c3eaaf8ffd2ca6617c4addf80a4df9
Rajesh Pal <tec28425@adobe.com>
2026-02-17 15:58:55 +0530

    Small card block

--- blocks/small-cards/small-cards.js
+++ blocks/small-cards/small-cards.js
@@ -1,231 +1,138 @@
 import {
   getBlockConfigs,
   getFieldValue,
-  handleDecide,
+  getBlockRepeatConfigs,
 } from '../../scripts/utils.js';
+
+/**
+ * Unifies the height of title containers based on the tallest content.
+ * Sets a minimum height for all title containers to ensure consistent layout.
+ * @param {HTMLElement} block - The parent block element containing the title containers.
+ */
+const setUnifiedHeight = (block) => {
+  // Select all title headings and their parent containers
+  const titleHeadings = block.querySelectorAll('.chart-advanced .title > h4');
+  const titleContainers = block.querySelectorAll('.chart-advanced .title');
+
+  /**
+   * Calculates the maximum height among all title headings.
+   * @returns {number} The maximum height in pixels.
+   */
+  const getMaxHeight = () => Array.from(titleHeadings).reduce((max, heading) => {
+    const contentHeight = heading.scrollHeight;
+    return contentHeight > max ? contentHeight : max;
+  }, 0);
+
+  /**
+   * Applies or removes minimum height to title containers based on screen width.
+   * @param {number} height - The height to apply as minimum height.
+   */
+  const applyMinHeight = (height) => {
+    const shouldApply = window.innerWidth >= 730;
+    titleContainers.forEach((container) => {
+      container.style.minHeight = shouldApply ? `${height}px` : '';
+    });
+  };
+
+  /**
+   * Throttles a function to limit its execution rate.
+   * @param {Function} func - The function to throttle.
+   * @param {number} delay - The delay in milliseconds.
+   * @returns {Function} The throttled function.
+   */
+  const throttle = (func, delay) => {
+    let timeoutId;
+    let lastExecTime = 0;
+    return (...args) => {
+      const currentTime = Date.now();
+      if (currentTime - lastExecTime > delay) {
+        func.apply(this, args);
+        lastExecTime = currentTime;
+      } else {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+          func.apply(this, args);
+          lastExecTime = Date.now();
+        }, delay - (currentTime - lastExecTime));
+      }
+    };
+  };
+
+  /**
+   * Handles window resize events to update title container heights dynamically.
+   */
+  const handleResize = () => {
+    const newMaxHeight = getMaxHeight();
+    applyMinHeight(newMaxHeight);
+  };
+
+  // Initial setup
+  const initialMaxHeight = getMaxHeight();
+  applyMinHeight(initialMaxHeight);
+
+  // Add throttled resize event listener
+  window.addEventListener('resize', throttle(handleResize, 300));
+};
 
 // DEFAULT
 const DEFAULT_CONFIG = {
-  desktopAlignment: 'center',
-  tabletAlignment: 'center',
-  mobileAlignment: 'center',
-  categoryRichtext: '',
-  titleRichtext: '',
-  bodyRichtext: '',
-  disclaimerRichtext: '',
-  motion: 'off',
-  ctaText: '',
-  ctaVisible: 'show',
-  ctaLinkType: '',
-  categoryIcon: '',
-  categoryIconNum: '',
-  categoryIconPosition: '',
-  categoryIcon1: '',
-  categoryIcon2: '',
-  categoryFont: 'tt-md-20',
-  categoryFontColor: '',
-  titleFont: 'tt-md-40',
-  titleFontColor: '',
-  bodyFontDT: 'ro-rg-20-md',
-  bodyFontM: 'ro-rg-20',
-  bodyFontColor: '',
-  disclaimerFontColor: '',
-  ctaFontDT: 'ro-md-20-md',
-  ctaFontM: 'ro-md-20',
-  ctaFontColor: '',
-  ctaHyperlink: '',
-};
-
-const handleCategoryClass = (num) => {
-  switch (num) {
-    case 1:
-      return 'items-center';
-    case 2:
-      return 'items-center flex-row-reverse';
-    case 3:
-      return 'flex-col';
-    case 4:
-      return 'flex-col flex-col-reverse';
-    case 5:
-      return 'flex-col flex-col-reverse md:items-center';
-    case 6:
-      return 'flex-col md:items-center';
-    default:
-      return 'items-center';
-  }
-};
-
-/**
- * Sets up animation logic for elements with the class `.text-block-animation`.
- * Observes when these elements enter the viewport and applies animations to their child elements.
- * @param {HTMLElement} block - The parent block element containing animated elements.
- */
-const setupAnimation = (block) => {
-  const containers = block.querySelectorAll('.text-block-animation');
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        let delay = 100;
-        const spans = entry.target.querySelectorAll('span');
-        spans.forEach((span) => {
-          delay += 100;
-          setTimeout(() => {
-            if (span?.style) {
-              span.style.opacity = 1;
-              span.style.transform = 'translateZ(0px) translateY(0px)';
-            }
-          }, delay);
-        });
-        const imgs = entry.target.querySelectorAll('img');
-        imgs.forEach((img) => {
-          delay += 100;
-          setTimeout(() => {
-            if (img?.style) {
-              img.style.opacity = 1;
-              img.style.transform = 'translateZ(0px) translateY(0px)';
-            }
-          }, delay);
-        });
-        const divs = entry.target.querySelectorAll('div');
-        divs.forEach((div) => {
-          delay += 100;
-          setTimeout(() => {
-            if (div?.style) {
-              div.style.opacity = 1;
-              div.style.transform = 'translateZ(0px) translateY(0px)';
-            }
-          }, delay);
-        });
-      }
-    });
-  });
-
-  containers.forEach((item) => {
-    observer.observe(item);
-  });
-};
-
-/**
- * Handles the execution of animations based on document readiness.
- * Ensures animations are triggered only after the document is fully loaded.
- * @param {HTMLElement} block - The parent block element containing animated elements.
- */
-const handleMotion = (block) => {
-  // Check if the document is still loading
-  if (document.readyState === 'loading') {
-    // If still loading, wait for the DOMContentLoaded event
-    block.addEventListener('DOMContentLoaded', () => {
-      setupAnimation(block);
-    });
-  } else {
-    // If already loaded, execute the animation setup immediately
-    setTimeout(() => {
-      setupAnimation(block);
-    }, 0);
-  }
+  wrapLine: 'on',
+  titleFont: 'tt-bd-28',
+  infoFont: 'ro-rg-18-sh',
+  lineWidth: '100%',
 };
 
 export default async function decorate(block) {
   try {
-    // return;
-    // console.log('执行small-cards', block);
-    const config = await getBlockConfigs(block, DEFAULT_CONFIG, 'small-cards');
-    const v = getFieldValue(config);
-    // console.log('执行small-cards-config', config);
+    const wrapper = block.querySelectorAll(':scope > div');
+    // Process each wrapper asynchronously
+    Array.from(wrapper).forEach(async (wrap) => {
+      try {
+        const config = await getBlockConfigs(wrap, DEFAULT_CONFIG, 'small-card');
+        const v = getFieldValue(config);
 
-    const dAlignment = v('desktopAlignment') === 'left' ? 'lg:items-start' : 'lg:items-center';
-    const tAlignment = v('tabletAlignment') === 'left' ? 'md:items-start' : 'md:items-center';
-    const mAlignment = v('mobileAlignment') === 'left' ? 'items-start' : 'items-center';
-    const dBlockAlignment = v('desktopAlignment') === 'left' ? 'lg:text-left' : 'lg:text-center';
-    const tBlockAlignment = v('tabletAlignment') === 'left' ? 'md:text-left' : 'md:text-center';
-    const mBlockAlignment = v('mobileAlignment') === 'left' ? 'text-left' : 'text-center';
+        const titleFontColor = v('titleFontColor') ? `--chart-advanced-title-color: #${v('titleFontColor')}` : '';
+        const infoFontColor = v('infoFontColor') ? `--chart-advanced-info-color: #${v('infoFontColor')}` : '';
+        const lineColor = v('lineColor') ? `--chart-advanced-line-bg-color: #${v('lineColor')}` : '';
+        // Retrieve repeated item configurations
+        const [items = []] = getBlockRepeatConfigs(wrap);
+        const itemHtml = items.map((val) => {
+          try {
+            return `
+              <div class="flex items-center gap-[20px]">
+                <div class="w-[46px] h-[46px] flex items-center justify-center shrink-0">${val.iconAssets.html}</div>
+                <span class="${v('infoFont')} chart-advanced-info" style="${infoFontColor}">${val.infoRichtext.text}</span> 
+              </div>
+            `;
+          } catch (error) {
+            // eslint-disable-next-line no-console
+            console.error('Error:', error);
+            return '';
+          }
+        }).join('');
 
-    let icon1 = '';
-    let icon2 = '';
-    let categoryClass = '';
-    let categoryFontColor = '';
-    const titleFontColor = v('titleFontColor') ? `style='--text-block-title-color: #${v('titleFontColor')};--text-block-title-gradient: '';'` : '';
-    const bodyFontColor = v('bodyFontColor') ? `style='--text-block-body-color: #${v('bodyFontColor')};'` : '';
-    const disclaimerFontColor = v('disclaimerFontColor') ? `style='--text-block-disclaimer-color: #${v('disclaimerFontColor')};'` : '';
-    let motion = '';
-    let ctaFontColor = '';
-
-    if (v('ctaLinkType') === 'text-link' && v('ctaFontColor')) {
-      const [start, end = start] = v('ctaFontColor').split(',');
-      ctaFontColor = `style='--text-block-cta-start: #${start}; --text-block-cta-end: #${end};'`;
-    }
-    if (v('motion') === 'on') {
-      motion = 'text-block-animation';
-      handleMotion(block);
-    }
-    try {
-      if (v('categoryFontColor')) {
-        categoryFontColor = `style='--text-block-category-color: #${v('categoryFontColor')};'`;
+        wrap.classList.add(`${v('chartColumnWidth') ? 'md:flex-none' : 'md:flex-1'}`, 'chat-column-width');
+        wrap.style.setProperty('--chart-advanced-chat-column-width', `${v('chartColumnWidth')}%`);
+        wrap.innerHTML = `
+          <div class="title"><h4 class="break-all ${v('titleFont')} chart-advanced-title" style="${titleFontColor}">${v('titleRichtext', 'html')}</h4></div>
+          <div class="h-[1px] mx-auto my-[16px] line-bg" style="width: ${v('lineWidth')}; ${lineColor}"></div>
+          <div class="break-all flex ${v('wrapLine') === 'on' ? 'flex-wrap' : 'flex-col'} content-start items-start gap-[16px]">
+            ${itemHtml}
+          </div>
+        `;
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Error decorating wrapper:', error);
       }
-      await handleDecide(v('categoryIcon') === 'yes');
-      await handleDecide(v('categoryIcon1'));
-      icon1 = `
-        <div class='w-[36px] h-[36px] flex items-center justify-center'>${v('categoryIcon1', 'html')}</div>
-      `;
-      categoryClass = handleCategoryClass(v('categoryIconPosition'));
-      await handleDecide(v('categoryIconNum') === 2 && v('categoryIcon2'));
-      icon2 = `
-        <div class='w-[36px] h-[36px] flex items-center justify-center'>${v('categoryIcon2', 'html')}</div>
-      `;
-      categoryClass = handleCategoryClass(7);
-    } catch (error) {
-      // console.warn('Error:', error);
-    }
+    });
 
-    const category = `
-      <div class='flex ${categoryClass}'>
-        ${icon1}
-        <div class='break-all flex-1 text-block-category ${v('categoryFont')}' ${categoryFontColor}>${v('categoryRichtext', 'html') || ''}</div>
-        ${icon2}
-      </div>
-    `;
-
-    const title = `
-      <div class='${v('titleRichtext') && 'mt-[10px]'} break-all text-block-title ${dBlockAlignment} ${tBlockAlignment} ${mBlockAlignment} ${v('titleFont')}' ${titleFontColor}>
-        ${v('titleRichtext', 'html')}
-      </div>
-    `;
-
-    const body = `
-      <div class='${v('bodyRichtext') && 'mt-[18px]'} break-all text-block-body ${dBlockAlignment} ${tBlockAlignment} ${mBlockAlignment} ${v('bodyFontDT')} ${v('bodyFontM')}' ${bodyFontColor}>
-        ${v('bodyRichtext', 'html')}
-      </div>
-    `;
-
-    const disclaimer = `
-      <div class='${v('disclaimerRichtext') && 'mt-[16px]'} break-all text-block-disclaimer ro-rg-13 ${dBlockAlignment} ${tBlockAlignment} ${mBlockAlignment}' ${disclaimerFontColor}>
-        ${v('disclaimerRichtext', 'html')}
-      </div>
-    `;
-
-    const cta = `
-      <div class='${v('ctaVisible') === 'hide' ? '' : 'mt-[16px]'} break-all ${dBlockAlignment} ${tBlockAlignment} ${mBlockAlignment}'>
-        <a href="${v('ctaHyperlink')}" class="text-block-cta ${v('ctaFontDT')} ${v('ctaFontM')}" ${ctaFontColor} target="_blank">
-            ${v('ctaText')}
-          </a>
-      </div>
-    `;
-
-    const wrap = `
-      <div class='flex flex-col ${motion} ${dAlignment} ${tAlignment} ${mAlignment}'>
-        ${category}
-        ${title}
-        ${body}
-        ${disclaimer}
-        ${v('ctaVisible') === 'hide' ? '' : cta}
-      </div>
-    `;
-
-    block.innerHTML = wrap;
+    block.classList.add('flex', 'flex-col', 'md:flex-row', 'md:flex-nowrap', 'gap-[40px]', 'w-full', 'chart-advanced');
+    setTimeout(() => {
+      setUnifiedHeight(block);
+    }, 500);
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.error('Error decorating text-block block:', error);
-    block.innerHTML = '<div class="error-message">Failed to load text-block block</div>';
+    console.error('Error decorating chart-advanced block:', error);
+    block.innerHTML = '<div class="error-message">Failed to load chart-advanced block</div>';
   }
 }
