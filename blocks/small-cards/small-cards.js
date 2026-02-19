98de0afa71402f17aaacfaa44857cf0c686153cc
Rajesh Pal <tec28425@adobe.com>
2026-02-19 17:21:18 +0530

    Small card block

--- blocks/small-cards/small-cards.js
+++ blocks/small-cards/small-cards.js
@@ -1,6 +1,8 @@
 export default function decorate(block) {
+  /* eslint-disable-next-line no-console */
+  console.log('Decorating small-cards block', block.children);
   const [quoteWrapper] = block.children;
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
-  quoteWrapper.replaceChildren(blockquote);
+  quoteWrapper.replaceChildren('Hello World');
 }
