9e2344d8c3cc8cf07385d6cb374d3d79ae151b36
Ashwath S <as@transurban.com>
2026-02-20 12:16:07 +0530

    code check

--- blocks/footnote/footnote.js
+++ blocks/footnote/footnote.js
@@ -1,31 +1,55 @@
-import {
-  getBlockConfigs,
-  getFieldValue,
-  getBlockRepeatConfigs,
-} from '../../scripts/utils.js';
+import { getBlockConfigs, getFieldValue } from '../../scripts/utils.js';
 
-// DEFAULT
 const DEFAULT_CONFIG = {
   itemTextFont: 'ro-rg-13',
 };
 
 export default async function decorate(block) {
   try {
-    const wrapper = block.querySelectorAll(':scope > div');
+    const wrappers = Array.from(block.querySelectorAll(':scope > div'));
 
-    Array.from(wrapper).forEach(async (wrap) => {
-      try {
-        const config = await getBlockConfigs(wrap, DEFAULT_CONFIG, 'footnoteitem');
-        const v = getFieldValue(config);
-        const [items = []] = getBlockRepeatConfigs(wrap);
-        // eslint-disable-next-line no-console
-        console.log('footnoteblock', block, config, v, items, 'end');
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error('Error decorating wrapper:', error);
-      }
+    block.innerHTML = `
+      <div class="footnotes">
+        <ol class="footnote flex flex-col gap-[10] list-decimal pl-[25]"></ol>
+      </div>
+    `;
+    const list = block.querySelector('.footnote');
+    const configs = await Promise.all(
+      wrappers.map((wrap) => getBlockConfigs(wrap, DEFAULT_CONFIG, 'footnoteitem')),
+    );
+
+    configs.forEach((config, idx) => {
+      const v = getFieldValue(config);
+
+      const index = idx + 1;
+      const id = v('idText') || `footnote-${index}`;
+      const text = v('textRichtext') || '';
+      const showArrow = v('arrowConfig') !== 'off';
+
+      const styleVars = [
+        v('arrowDefaultColor') ? `--footnote-itemarrow-default-color:#${v('arrowDefaultColor')};` : '',
+        v('arrowHoverColor') ? `--footnote-itemarrow-hover-color:#${v('arrowHoverColor')};` : '',
+        v('arrowPressColor') ? `--footnote-itemarrow-press-color:#${v('arrowPressColor')};` : '',
+      ].join('');
+
+      list.insertAdjacentHTML(
+        'beforeend',
+        `
+        <li class="footnote-item ${DEFAULT_CONFIG.itemTextFont}"
+            id="${id}"
+            data-footnote-index="${index}"
+            tabindex="0">
+          ${text}
+          ${showArrow
+    ? `<a class="footnote-arrow" href="#" aria-label="back to ${id} contents">
+                   <span class="ro-rg-18" style="${styleVars}">↑</span>
+                 </a>`
+    : ''
+}
+        </li>
+      `,
+      );
     });
-    // block.innerHTML = '';
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Error decorating footnote block:', error);
