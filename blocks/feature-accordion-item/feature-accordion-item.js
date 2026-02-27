e43727147050e26274fc844155c355df5c9a7574
Ashwath S <as@transurban.com>
2026-02-27 11:02:01 +0530

    fragment changes

--- blocks/feature-accordion-item/feature-accordion-item.js
+++ blocks/feature-accordion-item/feature-accordion-item.js
@@ -1,16 +1,16 @@
-import { loadFragment } from '../fragment/fragment.js';
+// import { loadFragment } from '../fragment/fragment.js';
 
 export default async function decorate(block) {
   try {
     // eslint-disable-next-line no-console
     console.log('feature-accordion-item', block, block.children);
-    const fragmentUrl = './media-block-fragment';
-    const path = fragmentUrl.startsWith('http')
-      ? new URL(fragmentUrl, window.location).pathname
-      : fragmentUrl;
+    // const fragmentUrl = './media-block-fragment';
+    // const path = fragmentUrl.startsWith('http')
+    //   ? new URL(fragmentUrl, window.location).pathname
+    //   : fragmentUrl;
 
-    const fragment = await loadFragment(path);
-    console.log('fragment', fragment, fragment.childNodes);
+    // const fragment = await loadFragment(path);
+    // console.log('fragment', fragment, fragment.childNodes);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Error decorating feature-accordion-item block:', error);
