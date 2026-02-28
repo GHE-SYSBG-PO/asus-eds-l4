9e99cd2d0851d6b8130f3ba0a85dd935069572dc
Rajesh Pal <tec28425@adobe.com>
2026-02-28 12:03:27 +0530

    feat/54-small-card-block-13

--- blocks/small-cards/small-cards.js
+++ blocks/small-cards/small-cards.js
@@ -335,7 +335,6 @@ function getMediaHTML(data) {
     content = `
             <div class="wditems__content wditems__content__0 theme-white" id="SectionID-tab-1" style="${style} background-color: var(--ai-noise-container-bg);">
         
-        <div class="item__media item__media--aiNoise">
           <div class="ai__noise__container" id="conferenceS1span3__aiNoiseContainer">
             
             <div class="nav__replay">
@@ -400,7 +399,6 @@ function getMediaHTML(data) {
               </audio>
             </div>
           </div>
-        </div>
       </div>`;
   } else if (mediaType === 'video') {
     const isVideoAutoPlay = String(videoAutoPlay).toLowerCase() === 'true';
