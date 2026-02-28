ff7d17492f10c34baa96a025a4be3ed5bd54f4ad
Yipeng He <she50877@adobe.com>
2026-02-28 16:42:28 +0800

    fix:bug

--- blocks/featureitem-block/featureitem-block.js
+++ blocks/featureitem-block/featureitem-block.js
@@ -77,6 +77,69 @@ import {
 //   block.classList.add(mode);
 // }
 
+// const FONTS = {
+//   asus: {
+//     categoryFontD: 'tt-md-20-lg',
+//     categoryFontT: 'tt-md-20-md',
+//     categoryFontM: 'tt-md-16-sm',
+//     titleFontD: 'tt-md-40-lg',
+//     titleFontT: 'tt-md-40-md',
+//     titleFontM: 'tt-md-40-sm',
+//     bodyFontD: 'ro-rg-20-lg',
+//     bodyFontT: 'ro-rg-20-md',
+//     bodyFontM: 'ro-rg-18-sm',
+//     disclaimerFont: 'ro-rg-13',
+//     ctaFontD: 'ro-md-20-sh-lg',
+//     ctaFontT: 'ro-md-20-sh-md',
+//     ctaFontM: 'ro-md-18-sh-sm',
+//   },
+//   proart: {
+//     categoryFontD: 'tt-md-20-lg',
+//     categoryFontT: 'tt-md-20-md',
+//     categoryFontM: 'tt-md-16-sm',
+//     titleFontD: 'tt-md-40-lg',
+//     titleFontT: 'tt-md-40-md',
+//     titleFontM: 'tt-md-40-sm',
+//     bodyFontD: 'ro-rg-20-lg',
+//     bodyFontT: 'ro-rg-20-md',
+//     bodyFontM: 'ro-rg-18-sm',
+//     disclaimerFont: 'ro-rg-13',
+//     ctaFontD: 'ro-md-20-sh-lg',
+//     ctaFontT: 'ro-md-20-sh-md',
+//     ctaFontM: 'ro-md-18-sh-sm',
+//   },
+//   rog: {
+//     categoryFontD: 'tg-bd-20-lg',
+//     categoryFontT: 'tg-bd-20-md',
+//     categoryFontM: 'tg-bd-16-sm',
+//     titleFontD: 'tg-bd-40-lg',
+//     titleFontT: 'tg-bd-40-md',
+//     titleFontM: 'tg-bd-40-sm',
+//     bodyFontD: 'rc-rg-20-lg',
+//     bodyFontT: 'rc-rg-20-md',
+//     bodyFontM: 'rc-rg-18-sm',
+//     disclaimerFont: 'rc-rg-13',
+//     ctaFontD: 'rc-bd-20-sh-lg',
+//     ctaFontT: 'rc-bd-20-sh-md',
+//     ctaFontM: 'rc-bd-18-sh-sm',
+//   },
+//   tuf: {
+//     categoryFontD: 'dp-cb-20-lg',
+//     categoryFontT: 'dp-cb-20-md',
+//     categoryFontM: 'dp-cb-16-sm',
+//     titleFontD: 'dp-cb-40-lg',
+//     titleFontT: 'dp-cb-40-md',
+//     titleFontM: 'dp-cb-40-sm',
+//     bodyFontD: 'ro-rg-20-lg',
+//     bodyFontT: 'ro-rg-20-md',
+//     bodyFontM: 'ro-rg-18-sm',
+//     disclaimerFont: 'ro-rg-13',
+//     ctaFontD: 'ro-md-20-sh-lg',
+//     ctaFontT: 'ro-md-20-sh-md',
+//     ctaFontM: 'ro-md-18-sh-sm',
+//   },
+// };
+
 // default
 const DEFAULT_CONFIG = { };
 
