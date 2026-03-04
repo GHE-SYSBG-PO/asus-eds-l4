255ff6c799cf6880eb9af973f81c40469fd52eab
Yu Luo <she87373@adobe.com>
2026-03-04 13:40:43 +0800

    fix: 修复

--- blocks/tab-3column/tab-3column.js
+++ blocks/tab-3column/tab-3column.js
@@ -249,7 +249,7 @@ export default async function decorate(block) {
         data-icon="${tabIconEnabled}"
         ${inlineStyle ? `style="${inlineStyle.trim()}"` : ''}
       >
-        <div class="tab3col-tab-bar w-auto sm:max-w-[172px] md:max-w-none md:h-[56px lg:w-[185px] ]">
+        <div class="tab3col-tab-bar">
           <button class="tab3col-arrow tab3col-arrow--prev" aria-label="Scroll tabs left" style="display:none">
             <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/></svg>
           </button>
@@ -303,7 +303,9 @@ export default async function decorate(block) {
     block.append(componentHtml);
 
     // default show
-    block.querySelector('.tab-3column-item').classList.add('is-active');
+    if (block.querySelector('.tab-3column-item')) {
+      block.querySelector('.tab-3column-item').classList.add('is-active');
+    }
 
     setupInteraction(block.querySelector('.tab3col-component'), motionEnabled);
 
