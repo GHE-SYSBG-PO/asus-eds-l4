c3a96ad2e782bd66e9eb2215c837425b2dbf7cdf
Ashwath S <as@transurban.com>
2026-03-05 10:46:03 +0530

    code revamp

--- blocks/feature-accordion/feature-accordion.js
+++ blocks/feature-accordion/feature-accordion.js
@@ -1,14 +1,14 @@
 export default function decorate(block) {
   try {
     console.log('feature accordion section', block);
-    const [col1, col2] = [...block.children];
-    const config = block.dataset;
-    if (col1) {
-      Object.entries(config).forEach(([key, value]) => {
-        col1.dataset[key] = value;
-      });
-    }
-    block.append(col1, col2);
+    // const [col1, col2] = [...block.children];
+    // const config = block.dataset;
+    // if (col1) {
+    //   Object.entries(config).forEach(([key, value]) => {
+    //     col1.dataset[key] = value;
+    //   });
+    // }
+    // block.append(col1, col2);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Error decorating feature-accordion block:', error);
