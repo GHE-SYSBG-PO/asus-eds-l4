274ac4197f2c44f92aa5b85399b7addea0b76f1d
Yipeng He <she50877@adobe.com>
2026-03-05 11:41:25 +0800

    feat: ID: 0 complete

--- blocks/section/section.js
+++ blocks/section/section.js
@@ -1,6 +1,25 @@
 export default async function decorate(block) {
   try {
     console.log(block);
+    const config = block.dataset;
+    console.log(config);
+    const sectionid = config.sectionid || '';
+    const bgcolor = config.bgcolor || '';
+    const paddingleft = config.paddingleft || '';
+    const paddingright = config.paddingright || '';
+
+    if (sectionid) {
+      block.id = sectionid;
+    }
+    if (bgcolor) {
+      block.style.backgroundColor = `#${bgcolor}`;
+    }
+    if (paddingleft) {
+      block.style.paddingLeft = paddingleft;
+    }
+    if (paddingright) {
+      block.style.paddingRight = paddingright;
+    }
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Error decorating section:', error);
