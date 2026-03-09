4489625c178faa79132fbc601e26df78a1c64f34
Yipeng He <she50877@adobe.com>
2026-03-09 09:27:25 +0800

    fix: update section

--- components/section/section.js
+++ components/section/section.js
@@ -1,20 +1,24 @@
+import { loadCSS } from '../../scripts/aem.js';
+
 export default function buildSecitonClass(block) {
+  loadCSS(`${window.hlx.codeBasePath}/components/section/section.css`);
   const config = block.dataset;
   const sectionid = config.sectionid || '';
   const bgcolor = config.bgcolor || '';
   const paddingleft = config.paddingleft || '';
   const paddingright = config.paddingright || '';
 
+  block.classList.add('section-block');
   if (sectionid) {
     block.id = sectionid;
   }
   if (bgcolor) {
-    block.style.backgroundColor = `#${bgcolor}`;
+    block.style.setProperty('--section-bg-color', `#${bgcolor}`);
   }
   if (paddingleft) {
-    block.style.paddingLeft = paddingleft;
+    block.style.setProperty('--section-padding-left', paddingleft);
   }
   if (paddingright) {
-    block.style.paddingRight = paddingright;
+    block.style.setProperty('--section-padding-right', paddingright);
   }
 }
